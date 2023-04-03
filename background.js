d8ff1a0def186deb13c7b6db1f9e748f8c3fa31f
gopoma <gordono@unsa.edu.pe>
2023-04-03 17:37:36 -0500

    add chrome extension template

---
+++ background.js
@@ -0,0 +1,38 @@
+const saveObjectInLocalStorage = async function (obj) {
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.local.set(obj, function () {
+                resolve();
+            });
+        } catch (ex) {
+            reject(ex);
+        }
+    });
+};
+
+const gatObjectInLocalStorage = async function (obj) {
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.local.get(key, function (value) {
+                resolve(value);
+            });
+        } catch (ex) {
+            reject(ex);
+        }
+    });
+};
+
+chrome.runtime.onConnect.addListener(function (port) {
+    port.onMessage.addListener(async function ({ message }) {
+        if (message === "startscrap") {
+            const status = "start";
+            await saveObjectInLocalStorage(status);
+        }
+        if (message === "finish") {
+            //const status = await getObjectInLocalStorage("status");
+
+            //if (status == "start")
+            port.postMessage({ message: "nextpage" });
+        }
+    });
+});
